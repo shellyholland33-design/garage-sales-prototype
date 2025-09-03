25c72f42954628a0ad3f51d1bf785cc997ec482c
shellyholland33-design <shelly.holland33@gmail.com>
2025-09-03 13:16:04 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,12 @@
-const CACHE_NAME = "garage_sales_v6";
-
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./data.js",
-  "./manifest.json",
-  "./icons/icon_192.png",
-  "./icons/icon_512.png"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
+// sw-reset, unregister and wipe caches, then reload pages
+self.addEventListener("install", e => self.skipWaiting());
 self.addEventListener("activate", e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", e => {
-  const req = e.request;
-  e.respondWith(
-    caches.match(req).then(cached => cached || fetch(req))
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll().then(clients => {
+        for (const c of clients) c.navigate(c.url);
+      }))
   );
 });
+self.addEventListener("fetch", e => fetch(e.request));
